4e63748ff47791820cda7c3c277451ba823d5730
Connor Roelofsen <connorroelofsen15@gmail.com>
2021-08-13 10:41:51 +0200

    CSS for SingleCab component. -> Add text to SingleCab component. Create Klimatpaverkan component with CSS. Edit Modeller CSS, transition transform image. Import needed Klimatpaverkan image into porject. Import Klimatpaverkan component into SingleCab component.

---
+++ src/components/SingleCab/Klimatpaverkan/Klimatpaverkan.jsx
@@ -0,0 +1,34 @@
+// Library Imports:
+import "./klimatpaverkan.css";
+import React from "react";
+
+// Component Imports:
+
+// Library Imports:
+
+
+const Klimatpaverkan = () => {
+  return (
+    <>
+      <div className="flexbox-klimatpaverkan-outer-container">
+        <div className="flexbox-klimatpaverkan-text-container">
+          <div className="flexbox-klimatpaverkan-heading-container">
+            <h2 id="klimatpaverkan-heading2">
+              Förnybart drivmedel med låg klimatpåverkan
+            </h2>
+          </div>
+          <h4 id="klimatpaverkan-heading4">
+            Nya D-Max godkänd att köras på det fossilfria bränslet HVO100
+          </h4>
+          <div className="flexbox-klimatpaverkan-button-container">
+            <button type="button" id="klimatpaverkan-button">
+              Läs mer om HVO100
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Klimatpaverkan;
--- src/components/SingleCab/SingleCab.jsx
+++ src/components/SingleCab/SingleCab.jsx
@@ -7,6 +7,7 @@ import Grova from "./Grova/Grova";
 import Farger2 from "./Farger2/Farger2";
 import Betyder2 from "./Betyder2/Betyder2";
 import Personlig2 from "./Personlig2/Personlig2";
+import Klimatpaverkan from "./Klimatpaverkan/Klimatpaverkan";
 import Utrustningsniva from "./Utrustningsniva/Utrustningsniva";
 
 // Image Imports:
@@ -16,13 +17,22 @@ const SingleCab = () => {
   return (
     <>
       <div className="flexbox-singlecab-main-container">
-        <img src={MainImage} alt="main-xrs" id="single-cab-main-image" />
+        {/* <img src={MainImage} alt="main-xrs" id="single-cab-main-image" /> */}
+        <div className="flexbox-singlecab-text-container">
+          <h1 id="singlecab-heading1">
+            Single Cab
+          </h1>
+          <button type="button">
+            Testa D-Max single cab idag
+          </button>
+        </div>
       </div>
       <Grova />
       <Utrustningsniva />
       <Farger2 />
       <Betyder2 />
       <Personlig2 />
+      <Klimatpaverkan />
     </>
   )
 }
